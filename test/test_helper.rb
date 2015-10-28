79f7fc11a19b3b6e289eca38c0dca13166567d81
selfup <boudinot.regis@yahoo.com>
2015-10-28 09:16:46 -0600

    Adding vcr to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/pride'
+require 'webmock'
+require 'vcr'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  VCR.config do |config|
+    config.cassette_library_dir = 'test/cassettes'
+  end
 end
