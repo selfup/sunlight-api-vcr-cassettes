b58266690a44cd85c0e8691bfcdcf9beff1529f8
selfup <boudinot.regis@yahoo.com>
2015-10-28 10:43:28 -0600

    added model test for find_by chamber

--- test/models/legislator_test.rb
+++ test/models/legislator_test.rb
@@ -1,8 +1,8 @@
 require './test/test_helper'
 
 class LegislatorTest < ActiveSupport::TestCase
-  test '#find_by' do
-    VCR.use_cassette('legislator#find_by') do
+  test '#find_by gender' do
+    VCR.use_cassette('legislator#find_by gender') do
       legislators = Legislator.find_by(gender: 'F')
       legislator = legislators.first
 
@@ -10,6 +10,18 @@ class LegislatorTest < ActiveSupport::TestCase
       assert_equal Legislator, legislator.class
       assert_equal 'Joni', legislator.first_name
       assert_equal 'Ernst', legislator.last_name
+    end
+  end
+
+  test '#find_by chamber' do
+    VCR.use_cassette('legislator#find_by chamber') do
+      legislators = Legislator.find_by(chamber: 'senate')
+      legislator = legislators.first
+
+      assert_equal 20, legislators.count
+      assert_equal Legislator, legislator.class
+      assert_equal 'Benjamin', legislator.first_name
+      assert_equal 'Sasse', legislator.last_name
     end
   end
 end
